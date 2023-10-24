0ae568e52573a14dd6860e2ce8c806a92c964c23
Padraic O'Neill <pconeill@gmail.com>
2023-10-24 13:46:40 -0600

    working demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import './element'; // <-- import the web component
 
 @Component({
   selector: 'app-root',
---
+++ src/app/element.ts
@@ -0,0 +1,39 @@
+import { AffineSchemas } from "@blocksuite/blocks";
+import { EditorContainer } from "@blocksuite/editor";
+import { Workspace, Page, Schema } from "@blocksuite/store";
+import { LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+@customElement('custom-editor')
+export class CustomAffineEditor extends LitElement {
+   
+    @property() doc: any;
+    readonly workspace: Workspace;
+    readonly page: Page | null;
+
+    constructor() {
+        super();
+
+        const schema = new Schema();
+        schema.register(AffineSchemas);
+
+        this.workspace = new Workspace({ id: 'foo', schema });
+        this.page = this.workspace.createPage({id: 'test-id'});
+        this.page?.waitForLoaded().then(() => {
+            const pageBlockId = this.page?.addBlock('affine:page');
+            const noteId = this.page?.addBlock('affine:note', {}, pageBlockId);
+            this.page?.addBlock('affine:paragraph', {}, noteId);
+        });
+    }
+
+    override connectedCallback() {
+        const editor = new EditorContainer();
+        editor.page = this.page!;
+        this.appendChild(editor);
+    }
+
+    override disconnectedCallback() {
+        this.removeChild(this.children[0]);
+    }
+
+}
