ee0b34bf86df7e090dc681b71d2d04e6284242b3
Padraic O'Neill <pconeill@gmail.com>
2023-10-25 15:40:04 -0600

    gets page on reload, but doesn't render editor

--- src/app/pages/editor/editor.component.ts
+++ src/app/pages/editor/editor.component.ts
@@ -1,64 +1,115 @@
-import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
 // import '../../element'; // <-- import the web component
 import { Page, Schema, Workspace } from '@blocksuite/store';
 import { AffineSchemas } from '@blocksuite/blocks';
 import { EditorContainer } from '@blocksuite/editor';
 import * as Y from 'yjs';
+import { WebsocketProvider } from 'y-websocket';
+
+const schema = new Schema();
+schema.register(AffineSchemas);
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
-export class EditorComponent implements OnInit, AfterViewInit {
+export class EditorComponent implements OnInit, AfterViewInit, OnDestroy {
 
   workspace: Workspace;
-  page: Page;
+  page: Page | null;
   editor!: EditorContainer;
-  @ViewChild('container', {read: ElementRef, static: false})
+  @ViewChild('container', { read: ElementRef, static: false })
   container!: ElementRef;
   ydoc!: Y.Doc;
-  ytext!: Y.Text;
+  ytext!: Y.Array<any>;
 
   constructor(
     private renderer: Renderer2
   ) {
 
     this.ydoc = new Y.Doc();
-    this.ytext = this.ydoc.getText('v-text');
-    
-    const schema = new Schema();
-    schema.register(AffineSchemas);
+    this.ytext = this.ydoc.getArray('v-text');
+
     this.workspace = new Workspace({ id: 'foo', schema });
 
-    this.page = this.workspace.createPage({ id: 'test-id' });
-    
-    this.page?.waitForLoaded().then(() => {
-      const pageBlockId = this.page.addBlock('affine:page');
-      const noteId = this.page.addBlock('affine:note', {}, pageBlockId);
-      const page1 = this.page.addBlock('affine:paragraph', { title: "hello world" }, noteId);
-      const page2 = this.page.addBlock('affine:paragraph', { title: "goodbye" }, page1);
-      this.page.addBlock('affine:paragraph', { title: "goodbye" }, page2);
+    let data = localStorage.getItem("test") || null;
+    if (data) {
+      const jsonObject = JSON.parse(data);
+      const numberArray = Object.keys(jsonObject).map(key => jsonObject[key]);
+      const binary = new Uint8Array(numberArray);
+      Y.applyUpdate(this.workspace.doc, binary);
 
+      this.page = this.workspace.getPage("test-id") || null
+      console.log(this.page);
+    } else {
+      this.page = this.workspace.createPage({ id: 'test-id' });
+      console.log(this.page)
+      if (this.page) {
+        this.page?.waitForLoaded().then(() => {
+          const pageBlockId = this.page?.addBlock('affine:page');
+          const noteId = this.page?.addBlock('affine:note', {}, pageBlockId);
+          this.page?.addBlock('affine:paragraph', { title: "hello world" }, noteId);
+          Y.applyUpdate(this.workspace.doc, Y.encodeStateAsUpdate(this.workspace.doc))
+        });
+      }
+    }
 
-      // Y.applyUpdate(this.ydoc, Y.encodeStateAsUpdate(this.workspace.doc))
-    });
+    const wsProvider = new WebsocketProvider('ws://localhost:1234', 'my-roomname', this.workspace.doc)
+    wsProvider.on('status', (event: { status: any; }) => {
+      console.log(event.status) // logs "connected" or "disconnected"
+    })
   }
 
   ngOnInit(): void {
-    this.editor = new EditorContainer();
-    this.editor.page = this.page!;
-  }
-  
-  ngAfterViewInit(): void {
-    this.renderer.appendChild(this.container.nativeElement, this.editor);
+    if (this.page) {
+      console.log("create edtior")
+      this.editor = new EditorContainer();
+      this.editor.page = this.page;
+    }
   }
 
-  handleClick(): void {
-    const update = Y.encodeStateAsUpdate(this.ydoc);
-    console.log(update);
-    Y.applyUpdate(this.ydoc, update);
-    localStorage.setItem("test", JSON.stringify(update));
-    // console.log(Y.encodeStateAsUpdate(this.ydoc))
+  ngAfterViewInit(): void {
+    if (this.editor && this.page) {
+      console.log("render editor");
+      this.renderer.appendChild(this.container.nativeElement, this.editor);
+      this.monitorChanges();
+    }
   }
+
+  monitorChanges(): void {
+    this.workspace.doc.on('update', (updates: Uint8Array, origin: any, doc: Y.Doc, tr: Y.Transaction) => {
+      if (tr.local) {
+        const update = Y.encodeStateAsUpdate(this.workspace.doc);
+        Y.applyUpdate(this.workspace.doc, update);
+        localStorage.setItem("test", JSON.stringify(Y.encodeStateAsUpdate(this.workspace.doc)));
+      }
+    })
+
+    this.ydoc.on('update', (updates: Uint8Array, origin: any, doc: Y.Doc, tr: Y.Transaction) => {
+      console.log("ydoc update", this.page?.id)
+      // if (tr.local) {
+      console.log("ydoc updates here");
+
+      // const jsonString = JSON.stringify(Y.encodeStateAsUpdate(this.workspace.doc, updates));
+      // localStorage.setItem("test2", jsonString);
+      // }
+    })
+
+  }
+
+  ngOnDestroy(): void {
+
+    if (this.workspace.doc) {
+      this.workspace.doc.destroy();
+    }
+  }
+
+  // handleClick(): void {
+  //   const update = Y.encodeStateAsUpdate(this.workspace.doc);
+  //   console.log(update);
+  //   Y.applyUpdate(this.workspace.doc, update);
+  //   localStorage.setItem("test", JSON.stringify(Y.encodeStateAsUpdate(this.workspace.doc)));
+  //   // console.log(Y.encodeStateAsUpdate(this.ydoc))
+  // }
 }
